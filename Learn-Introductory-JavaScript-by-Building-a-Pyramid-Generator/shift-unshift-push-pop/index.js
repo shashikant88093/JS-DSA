9ff608829c289c2539b9715c0a8fb6dfce6a2e56
shashikant kumar <shashi94goswami@gmail.com>
2025-07-17 13:32:20 +0530

    added the pyramid problem

---
+++ Learn-Introductory-JavaScript-by-Building-a-Pyramid-Generator/shift-unshift-push-pop/index.js
@@ -0,0 +1,20 @@
+let arr = [1,2,3,4,5]
+
+let pusehed = arr.push(6)
+
+console.log(pusehed)
+console.log(arr,"push")
+let poped = arr.pop(6)
+
+console.log(poped)
+console.log(arr,"pop")
+
+let shifted = arr.shift()
+
+console.log(shifted)
+console.log(arr,"shift")
+
+let unshifted = arr.unshift(7)
+
+console.log(unshifted)
+console.log(arr,"unshifted")
